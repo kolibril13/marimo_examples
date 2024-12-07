7f9d23e7770d06aaf2be23b774583bcc6aa79e21
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2024-12-07 22:31:24 +0100

    tweaks

---
+++ simple_demo/hello_world.py
@@ -0,0 +1,15 @@
+
+import marimo
+
+__generated_with = "0.9.32"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def __():
+    print("hi")
+    return
+
+
+if __name__ == "__main__":
+    app.run()
